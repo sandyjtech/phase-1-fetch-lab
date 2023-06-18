091148ec4f15bb1729fcdb8eeb84816a320fe3fd
Sandra G <sandyjtech@gmail.com>
2023-06-18 06:50:25 -0700

    addeds return statement inside fetch method

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
-  .then((json) => console.log(json));
-  
+  .then((data) => {
+    renderBooks(data);
+  })
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(book => {   
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
@@ -17,4 +18,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+
 });
